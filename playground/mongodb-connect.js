49e680079f8b5d6b322d9de57741a0734217c15b
Svilen Ukalski <svilen.ukalski@tryzens.com>
2019-01-14 17:26:36 +0200

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //Destructoring assignment
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB Server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2))
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Svilen Ukalski',
+    //     age: 29,
+    //     location: 'Sofia'
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
+    db.close();
+});
